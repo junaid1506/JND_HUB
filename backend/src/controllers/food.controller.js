adb8af53f97b8c433c6e9a1ebe76b6172318e043
junaid1506 <junnu2757@gmail.com>
2026-02-12 18:20:36 +0530

    Multer Intialize

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -3,6 +3,7 @@ const foodModel = require("../models/food.model");
 async function createFood(req, res) {
   console.log(req.foodPartner);
   res.send("Food Item is created");
+  console.log(req.body);
 }
 
 module.exports = { createFood };
--- backend/src/routes/food.routes.js
+++ backend/src/routes/food.routes.js
@@ -2,12 +2,18 @@ const express = require("express");
 const foodRouter = express.Router();
 const authMiddleware = require("../middleware/auth.middleware");
 const foodController = require("../controllers/food.controller");
+const multer = require("multer");
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
 
 // POST /api/food/ [protected]
 
 foodRouter.post(
   "/",
   authMiddleware.authFoodPartnerMiddleware,
+  upload.single("video"),
   foodController.createFood,
 );
 
